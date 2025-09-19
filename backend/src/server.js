04d539aa0dfaa585776b18bc72803e0a3e51c95d
SakshiDixit <sakshidixit142004@gmail.com>
2025-09-19 12:17:13 +0530

    updated serverjs where changed the production

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,7 +15,7 @@ const __dirname = path.resolve();
 // a middleware that executes first before reaching any route
 
 // console.log("Hi I am outside middleware");
-if (process.env.NODE_ENV !== production) {
+if (process.env.NODE_ENV !== "production") {
   app.use(
     cors({
       origin: "http://localhost:5173",
@@ -30,7 +30,7 @@ app.use(express.json());
 app.use(rateLimiter);
 app.use("/api/notes", notesRoutes);
 
-if (process.env.NODE_ENV === production) {
+if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
