1d5ffac901eaf7b566653ed181fdd991c7e295a1
SakshiDixit <sakshidixit142004@gmail.com>
2025-09-19 11:51:42 +0530

    prepared for the deployemnt

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,17 +7,21 @@ import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import rateLimiter from "./middlewares/rateLimiter.js";
 import cors from "cors";
+import path from "path";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 // a middleware that executes first before reaching any route
 
 // console.log("Hi I am outside middleware");
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  })
-);
+if (process.env.NODE_ENV !== production) {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
 app.use(express.json());
 // app.use((req, res, next) => {
 //   console.log("hi i am inside the middleware");
@@ -26,6 +30,12 @@ app.use(express.json());
 app.use(rateLimiter);
 app.use("/api/notes", notesRoutes);
 
+if (process.env.NODE_ENV === production) {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 connectDB().then(() => {
   app.listen(PORT, () => {
     console.log("App is listening on port", PORT);
